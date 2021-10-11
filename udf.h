a50aa14ac5bddbde7ecfe3c58288aa4f5b3f27e0
ChenWH <1773904345@qq.com>
2021-10-12 00:13:41 +0800

    v2.0

---
+++ udf.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <math.h>
+#include <iomanip>
+#include <time.h>
+#include <fstream>
+#include <map>
+
+int maxInt(int x, int y){
+    return (x > y ? x : y);
+}
+
+int minInt(int x, int y){
+    return (x < y ? x : y);
+}
