8aced8204f5ba8c29339dd88404d4c673a3a6316
ChenWH <1773904345@qq.com>
2021-10-20 18:52:39 +0800

    2021.10.20

--- Matrix_raw.h
+++ Matrix_raw.h
@@ -1,13 +1,6 @@
 #include <iostream>
-#include <stdio.h>
-#include <cstdlib>
-#include <cstring>
-#include <cmath>
+#include <string.h>
 #include <math.h>
-#include <iomanip>
-#include <time.h>
-#include <fstream>
-#include <map>
 
 using namespace std;
 
@@ -15,92 +8,73 @@ class Matrix
 {
 private:
 	int row, col;
+	float* index;
 public:
-double** index;
-	Matrix(int x = 1, int y = 1) {
-		row = x;
-		col = y;
-		index = new double* [row];
-		for (int i = 0; i < row; ++i) {
-			index[i] = new double[col];
-		}
+	//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>构 造 函 数<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+	// 指定行列数构造
+	Matrix(int X = 1, int Y = 1) {
+		row = X;
+		col = Y;
+		index = (float *)malloc( row*col*sizeof( float ));
 	}
-
+	// Matrix类构造
 	Matrix(const Matrix& obj) {
 		row = obj.row;
 		col = obj.col;
-		index = new double* [row];
-		for (int i = 0; i < row; ++i) {
-			index[i] = new double[col];
-		}
-		for (int i = 0; i < row; ++i) {
-			for (int j = 0; j < col; ++j) {
-				index[i][j] = obj.index[i][j];
-			}
+		index = (float *)malloc( row*col*sizeof( float ));
+		memcpy(index, obj.index, row*col* sizeof( float ));
+	}
+	// 二维数组构造
+	Matrix(float **array, int X, int Y) {
+		row = X;
+		col = Y;
+		index = (float *)malloc( row*col*sizeof( float ));
+		for (int i = 0; i < row; ++i){
+			memcpy(index + i*col, array[i], col*sizeof( float ));
 		}
 	}
-
-	Matrix(double **array, int x, int y) {
-		row = x;
-		col = y;
-		index = new double* [row];
-		for (int i = 0; i < row; ++i) {
-			index[i] = new double[col];
-		}
-		for (int i = 0; i < row; ++i) {
-			for (int j = 0; j < col; ++j) {
-				index[i][j] = array[i][j];
-			}
-		}
+	// 连续数组构造
+	Matrix(float *array, int X, int Y) {
+		row = X;
+		col = Y;
+		index = (float *)malloc( row*col*sizeof( float ));
+		memcpy(index, array, row*col* sizeof( float ));
 	}
-
-	Matrix(double *array, int x) {
-		row = x;
-		col = 1;
-		index = new double* [row];
-		for (int i = 0; i < row; ++i) {
-			index[i] = new double[col];
-		}
-		for (int i = 0; i < row; ++i) {
-			index[i][0] = array[i];
-		}
+	// 复制构造
+	Matrix& operator=(const Matrix& obj) {
+		free(index);
+		row = obj.row;
+		col = obj.col;
+		index = (float *)malloc( row*col*sizeof( float ));
+		memcpy(index, obj.index, row*col* sizeof( float ));
+		return *this;
 	}
-
+	//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>析 构 函 数<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 	~Matrix() {
-		for (int i = 0; i < row; ++i) {
-			delete[] index[i];
-		}
-		delete[] index;
+		free(index);
 	}
-
+	//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>初 始 化<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+	// 单位阵初始化
 	void E() {
+		memset(index, 0, row*col*sizeof(float));
 		for (int i = 0; i < row; ++i) {
-			for (int j = 0; j < col; ++j) {
-				index[i][j] = 0;
-			}
-		}
-		for (int i = 0; i < row; ++i) {
-			index[i][i] = 1;
+			index[i + i*col] = 1;
 		}
 	}
-
+	// 全零初始化
 	void zero() {
-		for (int i = 0; i < row; ++i) {
-			for (int j = 0; j < col; ++j) {
-				index[i][j] = 0;
-			}
-		}
+		memset(index, 0, row*col*sizeof(float));
 	}
-
+	// 输入赋值
 	friend istream& operator>>(istream& is, Matrix& obj) {
 		for (int i = 0; i < obj.row; ++i) {
 			for (int j = 0; j < obj.col; ++j) {
-				is >> obj.index[i][j];
+				is >> obj.index[i*obj.col + j];
 			}
 		}
 		return is;
 	}
-
+	// 输出
 	friend ostream& operator<<(ostream& os, const Matrix& obj) {
 		if (obj.row == 0 || obj.col == 0) {
 			os << "ERROR!";          //不符合矩阵运算时自动报错
@@ -108,211 +82,181 @@ double** index;
 		else {
 			for (int i = 0; i < obj.row; ++i) {
 				for (int j = 0; j < obj.col; ++j) {
-					os << obj.index[i][j] << ' ';
+					os << obj.index[i*obj.col + j] << ' ';
 				}
 				os << endl;
 			}
 		}
 		return os;
 	}
-
+	//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>成 员 获 取<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+	// 行数
 	int Row() {
 		return row;
 	}
-
+	// 列数
 	int Col() {
 		return col;
 	}
-
-	double& operator()(int x, int y) {
-		return index[x][y];
+	// 索引元素
+	float& operator()(int X, int Y) {
+		return index[X*col + Y];
 	}
-
+	//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>基 本 运 算<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 	friend Matrix operator+(const Matrix& obj1, const Matrix& obj2) {
 		if (obj1.row != obj2.row || obj1.col != obj2.col) {
-			cout << "相加矩阵维数不同!" << endl;
-			Matrix tmp;
-			return tmp;
+			printf("相加矩阵维数不同！\n");
+			return obj1;
 		}
 		else {
 			Matrix tmp(obj1.row, obj1.col);
 			for (int i = 0; i < tmp.row; ++i) {
 				for (int j = 0; j < tmp.col; ++j) {
-					tmp.index[i][j] = obj1.index[i][j] + obj2.index[i][j];
+					tmp.index[i*obj1.col + j] = obj1.index[i*obj1.col + j] + obj2.index[i*obj2.col + j];
 				}
 			}
 			return tmp;
 		}
 	}
-
+	friend Matrix operator+(const Matrix& obj, const float X) {
+		Matrix tmp(obj.row, obj.col);
+		for (int i = 0; i < tmp.row; ++i) {
+			for (int j = 0; j < tmp.col; ++j) {
+				tmp.index[i*obj.col + j] = obj.index[i*obj.col + j] + X;
+			}
+		}
+		return tmp;
+	}
 	friend Matrix operator-(const Matrix& obj1, const Matrix& obj2) {
 		if (obj1.row != obj2.row || obj1.col != obj2.col) {
-			cout << "相减矩阵维数不同！" << endl;
-			Matrix tmp;
-			return tmp;
+			printf("相减矩阵维数不同！\n");
+			return obj1;
 		}
 		else {
 			Matrix tmp(obj1.row, obj1.col);
 			for (int i = 0; i < tmp.row; ++i) {
 				for (int j = 0; j < tmp.col; ++j) {
-					tmp.index[i][j] = obj1.index[i][j] - obj2.index[i][j];
+					tmp.index[i*obj1.col + j] = obj1.index[i*obj1.col + j] - obj2.index[i*obj2.col + j];
 				}
 			}
 			return tmp;
 		}
 	}
-
+	friend Matrix operator-(const Matrix& obj, const float X) {
+		Matrix tmp(obj.row, obj.col);
+		for (int i = 0; i < tmp.row; ++i) {
+			for (int j = 0; j < tmp.col; ++j) {
+				tmp.index[i*obj.col + j] = obj.index[i*obj.col + j] - X;
+			}
+		}
+		return tmp;
+	}
 	friend Matrix operator*(const Matrix& obj1, const Matrix& obj2) {
 		if (obj1.col != obj2.row) {
-			cout << "维数不符合矩阵相乘原则！" << endl;
-			Matrix tmp;
-			return tmp;
+			printf("维数不符合矩阵相乘原则！\n");
+			return obj1;
 		}
 		else {
 			Matrix tmp(obj1.row, obj2.col);
 			for (int i = 0; i < tmp.row; ++i) {
 				for (int j = 0; j < tmp.col; ++j) {
-					tmp.index[i][j] = 0;
+					tmp.index[i*obj2.col + j] = 0;
 					for (int k = 0; k < obj1.col; ++k) {
-						tmp.index[i][j] = tmp.index[i][j] + obj1.index[i][k] * obj2.index[k][j];
+						tmp.index[i*obj2.col + j] = tmp.index[i*obj2.col + j] + obj1.index[i*obj1.col + k] * obj2.index[k*obj2.col + j];
 					}
 				}
 			}
 			return tmp;
 		}
 	}
-
-	friend Matrix operator*(const Matrix& obj, const double x) {
+	friend Matrix operator*(const Matrix& obj, const float X) {
 		Matrix tmp(obj.row, obj.col);
 		for (int i = 0; i < tmp.row; ++i) {
 			for (int j = 0; j < tmp.col; ++j) {
-				tmp.index[i][j] = obj.index[i][j] * x;
+				tmp.index[i*tmp.col + j] = obj.index[i*obj.col + j] * X;
 			}
 		}
 		return tmp;
 	}
-
-	friend Matrix operator/(const Matrix& obj, const double& divisor) {
+	friend Matrix operator/(const Matrix& obj, const float& X) {
 		Matrix tmp(obj.row, obj.col);
 		for (int i = 0; i < tmp.row; ++i) {
 			for (int j = 0; j < tmp.col; ++j) {
-				tmp.index[i][j] = obj.index[i][j] / divisor;
+				tmp.index[i*tmp.col + j] = obj.index[i*obj.col + j] / X;
 			}
 		}
 		return tmp;
 	}
-
-	Matrix& operator=(const Matrix& obj) {
-		for (int i = 0; i < row; ++i) {
-			delete[]index[i];
-		}
-		delete[]index;
-		row = obj.row;
-		col = obj.col;
-		index = new double* [row];
-		for (int i = 0; i < row; ++i) {
-			index[i] = new double[col];
-		}
-		for (int i = 0; i < row; ++i) {
-			for (int j = 0; j < col; ++j) {
-				index[i][j] = obj.index[i][j];
-			}
-		}
-		return *this;
-	}
-
+	//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>矩 阵 变 换<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 	//矩阵转置
-	Matrix transposition() {
+	Matrix trans() {
 		Matrix tmp(col, row);
 		for (int i = 0; i < tmp.row; ++i) {
 			for (int j = 0; j < tmp.col; ++j) {
-				tmp.index[i][j] = index[j][i];
+				tmp.index[i*tmp.col + j] = index[j*col + i];
 			}
 		}
 		return tmp;
 	}
-
 	//分块矩阵
-	Matrix block(int x, int y, int new_row, int new_col) {
+	Matrix block(int X, int Y, int new_row, int new_col) {
 		Matrix tmp(new_row, new_col);
 		for (int i = 0; i < new_row; ++i) {
-			for (int j = 0; j < new_col; ++j) {
-				tmp.index[i][j] = index[x + i][y + j];
-			}
+			memcpy(tmp.index + i*new_col, index + (i + X)*col, new_col* sizeof( float ));
 		}
 		return tmp;
 	}
-
-	//合并分块矩阵
-	friend Matrix reshape(const Matrix A, const Matrix B, const Matrix C, const Matrix D) {
-		if (A.row + C.row != B.row + D.row || A.col + B.col != C.col + D.col) {
-			cout << "无法合并矩阵！" << endl;
-			return A;
+	// 矩阵赋值
+	void cpy(Matrix obj, int X, int Y) {
+		for (int i = 0; i < obj.row; ++i) {
+			memcpy(index + (i + X)*col + Y, obj.index + i*obj.col, obj.col* sizeof( float ));
 		}
-		Matrix tmp(A.row + C.row, A.col + B.col);
-		for (int i = 0; i < A.row; ++i) {
-			for (int j = 0; j < A.col; ++j) {
-				tmp.index[i][j] = A.index[i][j];
-			}
-		}
-		for (int i = 0; i < B.row; ++i) {
-			for (int j = 0, count_y = A.col; j < B.col; ++j, ++count_y) {
-				tmp.index[i][count_y] = B.index[i][j];
-			}
-		}
-		for (int i = 0, count_x = A.row; i < C.row; ++i, ++count_x) {
-			for (int j = 0; j < C.col; ++j) {
-				tmp.index[count_x][j] = C.index[i][j];
-			}
-		}
-		for (int i = 0, count_x = A.row; i < D.row; ++i, ++count_x) {
-			for (int j = 0, count_y = A.col; j < D.col; ++j, ++count_y) {
-				tmp.index[count_x][count_y] = D.index[i][j];
-			}
-		}
-		return tmp;
 	}
-
 	//行交换
-	void swaprow(const int i, const int j) {
-		double* tmp = index[i];
-		index[i] = index[j];
-		index[j] = tmp;
+	void swaprow(const int X, const int Y) {
+		float* tmp = (float *)malloc( col*sizeof( float ));
+		memcpy(tmp, index + X*col, col* sizeof( float ));
+		memcpy(index + X*col, index + Y*col, col* sizeof( float ));
+		memcpy(index + X*col, tmp, col* sizeof( float ));
 	}
-
 	//行变换
-	void rowmultik(int k, double x) {
+	void rowmultik(int X, float Y) {
 		for (int i = 0; i < col; ++i) {
-			index[k][i] *= x;
+			index[X*col + i] *= Y;
 		}
 	}
-
+	void exp(int X){
+		for (int i = 0; i < row*col; ++i){
+			index[i] = pow(index[i], X);
+		}
+	}
+	//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>复 杂 运 算<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 	//求矩阵任意元素的余子式
-	double cofactor(const int x, const int y) {
-		double tmp;
+	float cofactor(const int X, const int Y) {
+		float tmp;
 		Matrix M1(row - 1, row - 1);
 		for (int i = 0; i < row; ++i) {
-			if (i == x) {
+			if (i == X) {
 				continue;
 			}
 			for (int j = 0; j < col; ++j) {
-				if (j == y) {
+				if (j == Y) {
 					continue;
 				}
-				if (i < x) {
-					if (j < y) {
-						M1.index[i][j] = index[i][j];
+				if (i < X) {
+					if (j < Y) {
+						M1.index[i*M1.col + j] = index[i*col + j];
 					}
 					else {
-						M1.index[i][j - 1] = index[i][j];
+						M1.index[i*M1.col + j - 1] = index[i*col + j];
 					}
 				}
 				else {
-					if (j < y) {
-						M1.index[i - 1][j] = index[i][j];
+					if (j < Y) {
+						M1.index[(i - 1)*M1.col + j] = index[i*col + j];
 					}
 					else {
-						M1.index[i - 1][j - 1] = index[i][j];
+						M1.index[(i - 1)*M1.col + j - 1] = index[i*col + j];
 					}
 				}
 			}
@@ -320,17 +264,16 @@ double** index;
 		tmp = M1.value();
 		return tmp;
 	}
-
 	//求行列式的值
-	double value() {
+	float value() {
 		Matrix tmp(*this);
 		int count = 0;
 		int i, j, k;
-		double det = 1, tp;
+		float det = 1, tp;
 		for (i = 0; i < row; ++i) {
-			if (tmp.index[i][i] == 0) {
+			if (tmp.index[i*tmp.col + i] == 0) {
 				for (j = i + 1; j < col; ++j) {
-					if (tmp.index[j][i] != 0) {
+					if (tmp.index[j*tmp.col + i] != 0) {
 						tmp.swaprow(i, j);
 						++count;
 						break;
@@ -341,22 +284,21 @@ double** index;
 				}
 			}
 			for (j = i + 1; j < row; ++j) {
-				if (tmp.index[j][i] != 0) {
-					tp = tmp.index[j][i] / tmp.index[i][i];
+				if (tmp.index[j*tmp.col + i] != 0) {
+					tp = tmp.index[j*tmp.col + i] / tmp.index[i*tmp.col + i];
 					for (k = i; k < col; ++k) {
-						tmp.index[j][k] -= tp * tmp.index[i][k];
+						tmp.index[j*tmp.col + k] -= tp * tmp.index[i*tmp.col + k];
 					}
 				}
 			}
 		}
 		for (i = 0; i < row; ++i) {
-			det *= tmp.index[i][i];
+			det *= tmp.index[i*tmp.col + i];
 		}
 		if (count % 2 == 1)
 			det = -det;
 		return det;
 	}
-
 	//判断是否正定
 	bool ifposdef() {
 		for (int i = 1; i <= row; ++i) {
@@ -366,78 +308,80 @@ double** index;
 		}
 		return true;
 	}
-
 	//矩阵修正
-	void modify() {
-		double b1 = 0, tp;
+	void modifY() {
+		float b1 = 0, tp;
 		for (int i = 0; i < row; ++i) {
-			tp = 2 * index[i][i];
+			tp = 2 * index[i*col + i];
 			for (int j = 0; j < col; ++j) {
-				tp -= index[i][j];
+				tp -= index[i*col + j];
 			}
 			b1 = (tp < b1) ? tp : b1;
 		}
 		for (int i = 0; i < row; ++i) {
-			index[i][i] -= b1;
+			index[i*col + i] -= b1;
 		}
 	}
-
 	//求伴随矩阵
 	Matrix adjoint() {
 		Matrix tmp(row, col);
 		for (int i = 0; i < tmp.row; ++i) {
 			for (int j = 0; j < tmp.col; ++j) {
 				if ((i + j) % 2 == 0) {
-					tmp.index[i][j] = cofactor(j, i);
+					tmp.index[i*tmp.col + j] = cofactor(j, i);
 				}
 				else {
-					tmp.index[i][j] = cofactor(j, i) * (-1);
+					tmp.index[i*tmp.col + j] = cofactor(j, i) * (-1);
 				}
 			}
 		}
 		return tmp;
 	}
-
 	//求解线性方程组
-	Matrix solve(const Matrix b) {
+	bool solve(const Matrix B, Matrix X) {
+		bool flag = true;
 		Matrix A(*this);
-		Matrix b2(b);
 		int i, j;
 		for (i = 0; i < A.row; ++i) {
 			j = i;
-			while (j < A.row && A.index[j][i] == 0) {
+			while (j < A.row && A.index[j*A.col + i] == 0) {
 				++j;
 			}
+			/*
 			if (j == A.row) {
 				cout << "wrong" << endl;
-				A.modify();
-				return A.solve(b);
+				A.modifY();
+				return A.solve(B, X);
+			}
+			*/
+			if (j == A.row) {
+				flag = false;
+				return flag;
 			}
 			else {
 				if (i != j) {
 					A.swaprow(i, j);
-					b2.swaprow(i, j);
+					X.swaprow(i, j);
 				}
 			}
 			//将对角位置转换为1
-			double tmp = 1.0 / A.index[i][i];
-			b2.rowmultik(i, tmp);
+			float tmp = 1.0 / A.index[i*A.col + i];
+			X.rowmultik(i, tmp);
 			A.rowmultik(i, tmp);
 			//将该列非对角位置转换为0
 			for (int k = 0; k < A.row; k++) {
-				if (k == i || A.index[k][i] == 0) {
+				if (k == i || A.index[k*A.col + i] == 0) {
 					continue;
 				}
 				for (j = i + 1; j < A.col; ++j) {
 					A(k, j) -= A(i, j) * A(k, i);
 				}
-				b2(k, 0) -= b2(i, 0) * A(k, i);
+				X(k, 0) -= X(i, 0) * A(k, i);
 				A(k, i) = 0;
 			}
 		}
-		return b2;
+		return flag;
 	}
-
 	//求逆矩阵
 	Matrix inv() {
 		Matrix tmp(*this);
@@ -447,7 +391,7 @@ double** index;
 		for (i = 0; i < row; ++i) {
 			//判断对角方向的元素是否为0
 			j = i;
-			while (j < row && tmp.index[j][i] == 0) {
+			while (j < row && tmp.index[j*tmp.col + i] == 0) {
 				++j;
 			}
 			if (j == row) {
@@ -461,58 +405,64 @@ double** index;
 				}
 			}
 			//将对角位置转换为1
-			double tp = 1.0 / tmp(i, i);
+			float tp = 1.0 / tmp(i, i);
 			eye.rowmultik(i, tp);
 			tmp.rowmultik(i, tp);
 			//将该列非对角位置转换为0
 			for (k = 0; k < row; k++) {
-				if (k == i || tmp.index[k][i] == 0) {
+				if (k == i || tmp.index[k*tmp.col + i] == 0) {
 					continue;
 				}
 				for (j = i + 1; j < col; j++) {
-					tmp.index[k][j] -= tmp(i, j) * tmp(k, i);
+					tmp.index[k*tmp.col + j] -= tmp(i, j) * tmp(k, i);
 				}
 				for (j = 0; j < col; j++) {
-					eye.index[k][j] -= eye(i, j) * tmp(k, i);
+					eye.index[k*eye.col + j] -= eye(i, j) * tmp(k, i);
 				}
-				tmp.index[k][i] = 0;
+				tmp.index[k*tmp.col + i] = 0;
 			}
 		}
 		return eye;
 	}
-
-	//一维范数
-	double norm1() {
-		double tmp = 0;
-		for (int i = 0; i < row; ++i) {
-			for (int j = 0; j < col; ++j) {
-				tmp += fabs(index[i][j]);
+	//数组乘法
+	friend Matrix dot(Matrix obj1, Matrix obj2)
+	{
+		if (obj1.row != obj2.row || obj1.col != obj2.col) {
+			cout << "矩阵维数不相等！" << endl;
+			return obj1;
+		}
+		else {
+			Matrix tmp(obj1.row, obj1.col);
+			for (int i = 0; i < obj1.row; ++i){
+				for (int j = 0; j < obj1.col; ++j){
+					tmp(i, j) = obj1(i, j)*obj2(i, j);
+				}
 			}
+			return tmp;
+		}
+	}
+	//一维范数
+	float norm1() {
+		float tmp = 0;
+		for (int i = 0; i < row*col; ++i) {
+			tmp += fabs(index[i]);
 		}
 		return tmp;
 	}
-
 	//二维范数
-	double norm2() {
-		double tmp = 0;
-		for (int i = 0; i < row; ++i) {
-			for (int j = 0; j < col; ++j) {
-				tmp += index[i][j] * index[i][j];
-			}
+	float norm2() {
+		float tmp = 0;
+		for (int i = 0; i < row*col; ++i) {
+			tmp += pow(index[i], 2);
 		}
 		return sqrt(tmp);
 	}
-	
 	//求取两向量夹角
-	friend double theta(Matrix obj1, Matrix obj2)
+	friend float theta(Matrix obj1, Matrix obj2)
 	{
-		if (obj1.row != obj2.row || obj1.col != 1 || obj2.col != 1) {
-			cout << "向量维数不相等！" << endl;
-			return 0;
-		}
-		double tmp = 0;
-		for (int i = 0; i < obj1.row; ++i) {
-			tmp += obj1.index[i][0] * obj2.index[i][0];
+		float tmp = 0;
+		for (int i = 0; i < obj1.row*obj1.col; ++i) {
+			tmp += obj1.index[i] * obj2.index[i];
 		}
 		return tmp / obj1.norm2() / obj2.norm2();
 	}
